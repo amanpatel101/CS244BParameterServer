57cd0b9f661fb9078f95ea21df2550e3674e2ab6
Anusri Pampari <p.anushri25@gmail.com>
2022-05-13 10:20:08 -0700

    fixed keys in update weights and timing analaysis

---
+++ anusri/script.sh
@@ -0,0 +1,7 @@
+python parameter_server.py -ns 50 -o output
+python parameter_server.py -ns 20 -o output
+python parameter_server.py -ns 10 -o output
+python parameter_server.py -ns 5 -o output
+python parameter_server.py -ns 1 -o output
+
+
